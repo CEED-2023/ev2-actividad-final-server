e6542d4cf7a941efa9e92517c35a2abe47fa3fbd
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-01-30 07:05:54 +0100

    Receives level, not size, from client

--- src/routes/create_game_route.js
+++ src/routes/create_game_route.js
@@ -1,6 +1,14 @@
 import Game from "../game/game.js"
 import randomId from "../game/random_id.js"
 
+
+
+const SETTINGS = {
+  'EASY': { height: 9, width: 9, mines: 10 },
+  'MEDIUM': { height: 16, width: 16, mines: 40 },
+  'HARD': { height: 16, width: 30, mines: 99 },
+}
+
 async function routes(fastify, _options) {
 
   fastify.post(
@@ -8,7 +16,13 @@ async function routes(fastify, _options) {
     async (request, reply) => {
       try {
         const requestBody = request.body
-        const { player, height, width, mines } = requestBody
+        const { player, difficulty } = requestBody
+        console.log(`Creating game for ${player} with difficulty ${difficulty}`)
+
+        const gameSettings = SETTINGS[difficulty]
+        if(!gameSettings) return reply.send({ success: false, error: 'Invalid settings' })
+
+        const { height, width, mines } = SETTINGS[difficulty]
 
         const id = randomId()
         const game = new Game(player, height, width, mines)
@@ -18,7 +32,8 @@ async function routes(fastify, _options) {
 
         reply.code(200).send({
           success: true,
-          id
+          id,
+          height, width, mines
         })
       } catch (error) {
         console.error('Error processing JSON:', error)
